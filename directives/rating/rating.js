53add6aba17f4bfe5e96e7c1599aae80d59dd822
Shyam Seshadri <shyamseshadri@gmail.com>
2013-05-22 15:01:03 +0530

    Step 2 - Show filled stars based on bound rating value

--- directives/rating/rating.js
+++ directives/rating/rating.js
@@ -2,8 +2,19 @@ angular.module('FundooDirectiveTutorial', [])
   .directive('fundooRating', function () {
     return {
       restrict: 'A',
+      template: '<ul class="rating">' +
+                  '<li ng-repeat="star in stars" class="filled">' +
+                    '\u2605' +
+                  '</li>' +
+                '</ul>',
+      scope: {
+        ratingValue: '='
+      },
       link: function (scope, elem, attrs) {
-        console.log("Recognized the fundoo-rating directive usage");
+        scope.stars = [];
+        for (var  i = 0; i < scope.ratingValue; i++) {
+          scope.stars.push({});
+        }
       }
     }
   });
